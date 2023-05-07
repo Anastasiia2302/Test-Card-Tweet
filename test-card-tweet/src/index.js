b2d9194933a0032afe91ed2764fc66ceea2fc994
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-05-07 13:52:44 +0300

    basename

--- test-card-tweet/src/index.js
+++ test-card-tweet/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename='https://anastasiia2302.github.io/Test-Card-Tweet/' >
     <App />
     </BrowserRouter>
   </React.StrictMode>
