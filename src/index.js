8ce3af48e7fbae9183c57ac8312927942cbc5998
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-05-07 21:46:59 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  >
+    <BrowserRouter basename='/Test-Card-Tweet' >
     <App />
     </BrowserRouter>
   </React.StrictMode>
